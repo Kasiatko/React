7bff6c1dc99ebc7c4e27d58083b8dcc0f3f3d17b
Katarzyna Sek <kasia05678@gmail.com>
2023-02-27 13:42:54 +0100

    Creating Books App

---
+++ books/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react';
+import BookCreate from './components/BookCreate';
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+
+  const createBook = (title) => {
+    console.log("Need to add book with: ",title);
+  };
+
+  return (
+    <div>
+        <BookCreate onCreate={createBook} />
+    </div>
+  )
+}
+
+export default App

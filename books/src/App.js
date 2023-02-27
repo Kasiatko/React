a907d2a00a063286940760b258f7dafc1a63a400
Katarzyna Sek <kasia05678@gmail.com>
2023-02-27 17:52:28 +0100

    Displaying BookList and BooksShow components

--- books/src/App.js
+++ books/src/App.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import { useState } from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
   const [books, setBooks] = useState([]);
 
 
   const createBook = (title) => {
-    const updatedBooks = [
-        ...books, 
-        {id: 123, title: title}
-    ]
-
+    const updatedBooks = [...books, 
+        {
+        id: Math.round(Math.random() * 999), 
+        title
+    }]
+        //title:title jezeli wartosc i klucz jest taka sama wystarczy napisac raz
     setBooks(updatedBooks);
   };
 
   return (
-    <div>
+    <div className='app'>
+        <BookList books={books}/>
         <BookCreate onCreate={createBook} />
     </div>
   )

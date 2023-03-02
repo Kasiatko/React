55e4b3d76aca04b76f64974db89b8d0e55d2d853
Katarzyna Sek <kasia05678@gmail.com>
2023-03-02 19:47:12 +0100

    Api added

--- books/src/App.js
+++ books/src/App.js
@@ -32,7 +32,7 @@ function App() {
   
 
   const deleteBookById = async(id) => {
-    await axios.delete(`http://localhost:3001/books/${id}`)
+    await axios.delete(`http://localhost:3001/books/${id}`);
     const updatedList = books.filter((book) => {
         return book.id !== id;
     })

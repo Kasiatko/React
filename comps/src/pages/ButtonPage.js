7a383072f06e6dcaef3bdbc618550ce93aec19e8
Katarzyna Sek <kasia05678@gmail.com>
2023-03-07 19:13:20 +0100

    Accordion component added

---
+++ comps/src/pages/ButtonPage.js
@@ -0,0 +1,39 @@
+import {GoBell, GoCloudDownload, GoDatabase} from 'react-icons/go';
+import React from 'react';
+import Button from '../components/Button';
+
+function ButtonPage() {
+    const handleClick = () => {
+        console.log('click')
+    }
+  return (
+    <div>
+        <div>
+            <Button primary rounded onClick={handleClick} className='mb-5'>
+            <GoBell />
+            Click me!
+            </Button>
+        </div>
+        <div>
+            <Button secondary outline onMouseEnter={handleClick}>Buy Now!</Button>
+        </div>
+        <div>
+            <Button warning onMouseLeave={handleClick}>
+            <GoCloudDownload />
+            See Deal!
+            </Button>
+        </div>
+        <div>
+            <Button success outline >Hide Ads!</Button>
+        </div>
+        <div>
+            <Button danger rounded>
+            <GoDatabase />  
+            Go to ShoppingCart!
+            </Button>
+        </div>      
+    </div>
+  )
+}
+
+export default ButtonPage;

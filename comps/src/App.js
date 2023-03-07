c9e8f05aae321fa3fa79e6584a7756e4a6051eb0
Katarzyna Sek <kasia05678@gmail.com>
2023-03-07 17:25:25 +0100

    Buttons added

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,23 +1,37 @@
+import {GoBell, GoCloudDownload, GoDatabase} from 'react-icons/go';
 import React from 'react';
 import Button from './Button';
 
 function App() {
+
+    const handleClick = () => {
+        console.log('click')
+    }
   return (
     <div>
         <div>
-            <Button primary rounded>Click me!</Button>
+            <Button primary rounded onClick={handleClick} className='mb-5'>
+            <GoBell />
+            Click me!
+            </Button>
         </div>
         <div>
-            <Button secondary rounded outline>Buy Now!</Button>
+            <Button secondary outline onMouseEnter={handleClick}>Buy Now!</Button>
         </div>
         <div>
-            <Button warning outline>See Deal!</Button>
+            <Button warning onMouseLeave={handleClick}>
+            <GoCloudDownload />
+            See Deal!
+            </Button>
         </div>
         <div>
-            <Button success>Hide Ads!</Button>
+            <Button success outline >Hide Ads!</Button>
         </div>
         <div>
-            <Button danger outline>Go to ShoppingCart!</Button>
+            <Button danger rounded>
+            <GoDatabase />  
+            Go to ShoppingCart!
+            </Button>
         </div>      
     </div>
   )
--- comps/src/Button.js
+++ comps/src/Button.js
@@ -10,27 +10,32 @@ function Button({
   danger,
   outline,
   rounded,
+  ...rest
 }) {
 
-    const classes = className('px-3 py-1.5 border', {
-        'border-blue-500 bg-blue-500 text-white': primary,
-        'border-gray-900 bg-gray-900 text-white': secondary,
-        'border-green-500 bg-green-500 text-white': success,
-        'border-yellow-400 bg-yellow-400 text-white': warning,
-        'border-red-500 bg-red-500 text-white': danger,
-        'rounded-full': rounded,
-        'bg-white': outline,
-        'text-blue-500': outline && primary,
-        'text-gray-500': outline && secondary,
-        'text-green-500': outline && success,
-        'text-yellow-500': outline && warning,
-        'text-red-500': outline && danger,
+    const classes = className(rest.className,'flex items-center px-3 py-1.5 border', {
+      'border-blue-500': primary,
+      'border-gray-900': secondary,
+      'border-green-500': success,
+      'border-yellow-400': warning,
+      'border-red-500': danger,
+      'rounded-full': rounded,
+      'bg-white text-blue-500': outline && primary,
+      'bg-white text-gray-900': outline && secondary,
+      'bg-white text-green-500': outline && success,
+      'bg-white text-yellow-400': outline && warning,
+      'bg-white text-red-500': outline && danger,
+      'bg-blue-500 text-white': !!!outline && primary,
+      'bg-gray-900 text-white': !!!outline && secondary,
+      'bg-green-500 text-white': !!!outline && success,
+      'bg-yellow-400 text-white': !!!outline && warning,
+      'bg-red-500 text-white': !!!outline && danger,
     });
 
 
 
   return (
-    <button className={classes}>
+    <button {...rest} className={classes}>
       {children}
     </button>
   );

f223feb7e7070062e765f663136ca1f2ddd75b54
Katarzyna Sek <kasia05678@gmail.com>
2023-03-08 20:50:12 +0100

    Modal completed

--- comps/src/pages/ModalPage.js
+++ comps/src/pages/ModalPage.js
@@ -10,10 +10,42 @@ function ModalPage() {
         setShowModal(true);
     }
 
+    const handleClose = () => {
+        setShowModal(false)
+    }
+
+    const actionBar = (
+        <div>
+            <Button onClick={handleClose} primary>I accept</Button>
+        </div>
+    )
+
+    const modal = <Modal onClose={handleClose} actionBar={actionBar}>
+        <p>
+            Here is an important agreement for you to accept
+        </p>
+
+        </Modal>
+
   return (
-    <div>
+    <div className='relative'>
+        
+        
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        <p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+        </p>
+        
         <Button primary onClick={handleOnClick}>Open Modal</Button>
-        {showModal && <Modal />}
+        {showModal && modal }
     </div>
   )
 }

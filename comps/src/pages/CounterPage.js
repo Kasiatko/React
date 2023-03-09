492536fc191f75ead1523649a455b788f3cd8091
Katarzyna Sek <kasia05678@gmail.com>
2023-03-09 18:29:49 +0100

    App completed

--- comps/src/pages/CounterPage.js
+++ comps/src/pages/CounterPage.js
@@ -1,60 +1,47 @@
 import Panel from '../components/Panel'
 import Button from "../components/Button";
 import { useReducer } from "react";
+import produce from 'immer';
+
 
 const INCREMENT_COUNT = 'increment';
-const SET_VALUE_TO_ADD = 'change-value ';
+const SET_VALUE_TO_ADD = 'change_value_to_add ';
 const DECREMENT_COUNT = 'decrement';
-const ADD_VALUE_TO_COUNT = 'add-value';
+const ADD_VALUE_TO_COUNT = 'add_value_to_count';
 
 const reducer =(state, action) => {
     switch (action.type) {
         case INCREMENT_COUNT:
-            return {
-                    ...state,
-                    count: state.count + 1
-                    };
-        case SET_VALUE_TO_ADD:
-            return {
-                ...state,
-                valueToAdd: action.payload
-            }
+            state.count = state.count + 1;
+            return;
         case DECREMENT_COUNT:
-            return{
-                ...state,
-                count: state.count -1
-            }
+            state.count = state.count -1;
+            return;
         case ADD_VALUE_TO_COUNT: 
-            return {
-                ...state, 
-                count: state.count + state.valueToAdd,
-                valueToAdd: 0, 
-            }
+            state.count = state.count + state.valueToAdd;
+            state.valueToAdd = 0;
+            return;
+        case SET_VALUE_TO_ADD:
+            state.valueToAdd = action.payload;
+            return;        
         default:
-            return state;
-    }
-   
- 
+            return;
+    } 
 };
 
 function CounterPage({initialCount}) {
-    // const [count, setCount] = useState(initialCount);
-    // const [valueToAdd , setValueToAdd] = useState(0);
-
-    const [state, dispatch] = useReducer(reducer, {
+    const [state, dispatch] = useReducer(produce(reducer), {
         count:initialCount,
         valueToAdd: 0,
     });
 
     const increment = () => {
-        // setCount(count + 1);
         dispatch({
             type: INCREMENT_COUNT
         });
     };
 
     const decrement = () => {
-        // setCount(count - 1);
         dispatch({
             type: DECREMENT_COUNT
         })
@@ -70,8 +57,6 @@ function CounterPage({initialCount}) {
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        // setCount(count + valueToAdd);
-        // setValueToAdd(0);
         dispatch({
             type: ADD_VALUE_TO_COUNT,
         })

8a6ecef61eaf85f9a776326ea701e681a1d405c6
Katarzyna Sek <kasia05678@gmail.com>
2023-03-10 14:56:31 +0100

    Initial App

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,54 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-import { configureStore } from "@reduxjs/toolkit";
-
-const initialMovies = {
-  movies:[
-    'Rambo III', 'Hakerzy', 'Matrix'
-  ]
-}
-
-function movies(state = initialMovies , action) {
-  switch (action.type) {
-    case 'ADD':
-      return {
-        ...state, movies: [...state.movies, action.movie]
-      }
-    case 'DELETE':
-      return{
-        ...state, movies: state.movies.filter(item => item !== action.movie)
-      }
-    case 'RESET':
-      return {
-        movies: []
-      }
-    default:
-      return state
-  }
-}
-
-const store = configureStore({reducer: movies})
-window.store = store
+import React from 'react';
+import CarForm from './components/CarForm';
+import CarList from './components/CarList';
+import CarSearch from './components/CarSearch';
+import CarValue from './components/CarValue';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <CarForm/>
+        <CarList />
+        <CarSearch />
+        <CarValue/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
